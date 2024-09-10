6a0a76a81e9c56f27e952e4c6db45712bfc61846
mydocuments <hamed.mr.unique@gmail.com>
2024-09-09 23:30:33 -0700

    adding karate config

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,6 +6,10 @@ public class TestRunner {
 
     @Karate.Test
     Karate runTest() {
-        return Karate.run("classpath:features/SecurityTest.feature");
+        return Karate
+                .run("classpath:features")
+                .karateEnv("dev")
+                .tags("@US_2");
+
     }
 }
